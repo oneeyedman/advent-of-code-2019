1e9af21b566f8447a587327c9d118f70682f6846
Carlos Mañas <carlos@spacenomads.com>
2019-12-05 22:39:07 +0100

    FORKING FINALLY

--- 02/js/main.js
+++ 02/js/main.js
@@ -1,7 +1,5 @@
 'use strict';
 
-// Input: https://adventofcode.com/2019/day/2/input
-//const puzzleURL = 'data/test-data.txt';
 const puzzleURL = 'data/data.txt';
 const result1 = document.querySelector('#result-1');
 const result2 = document.querySelector('#result-2');
@@ -15,12 +13,12 @@ const restore1202PAS = data => {
   data[2] = 2;
 };
 
-const run = instruction => {
+const run = (instruction, intcodeProgram) => {
   const [opcode, param1, param2, param3] = instruction;
-  const value1 = input[param1];
-  const value2 = input[param2];
+  const value1 = intcodeProgram[param1];
+  const value2 = intcodeProgram[param2];
   let value3 = (opcode === 1) ? value1+value2 : value1*value2;
-  input[param3] = value3;
+  intcodeProgram[param3] = value3;
 };
 
 const printProgram = codes => {
@@ -29,28 +27,55 @@ const printProgram = codes => {
 
   while (codes[counter] !== 99) {
     const intcode = codes.slice(counter,size);
-    run(intcode);
+    run(intcode, codes);
     counter += 4;
     size = counter + 4;
   }
-  return(input[0]);
+  return(codes[0]);
 };
 
+const getNounAndVerbs = () => {
+  const result = [];
+  for (let i=0; i<=99; i++) {
+    for (let j=0; j<=99; j++) {
+      result.push({
+        noun: i,
+        verb: j
+      });
+    }
+  }
+  return result;
+};
+
+const gravityAssistAroundTheMoon = data => {
+  const pairs = getNounAndVerbs();
+  let result;
+
+  for (let i=0; i<pairs.length; i++) {
+    const program = data.slice(0);
+    program[1] = pairs[i].noun;
+    program[2] = pairs[i].verb;
+    const address0 = printProgram(program);
+    if (address0 === output) {
+      const {noun, verb} = pairs[i];
+      result = 100 * noun + verb;
+    }
+  }
+  return result;
+};
 
 fetch(puzzleURL)
   .then(res => res.text())
   .then(puzzle => {
 
     input = handleData(puzzle);
+    const originalInput = input.slice(0);
     restore1202PAS(input);
-    //const originalInput = input.slice(0);
-
-    printProgram(input);
 
     // Part 1
     result1.innerHTML = printProgram(input);
 
     // Part 2
-    //result2.innerHTML = calculateTotalRequiredFuel(input);
+    result2.innerHTML = gravityAssistAroundTheMoon(originalInput);
 
   });
