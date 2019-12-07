de0114f847b99a0cd154bd835679a57cdd244106
Carlos Mañas <carlos@spacenomads.com>
2019-12-07 19:38:00 +0100

    Solve first problem

---
+++ 04/js/main.js
@@ -0,0 +1,55 @@
+'use strict';
+
+const puzzleURL = 'data/data.txt';
+const result1 = document.querySelector('#result-1');
+const result2 = document.querySelector('#result-2');
+let input;
+
+const handleData = data => {
+  const [rangeMin, rangeMax] = data.split('-');
+  const result=[];
+  for (let i=parseInt(rangeMin); i<=parseInt(rangeMax); i++) {
+    result.push(i);
+  }
+  return result;
+};
+
+const hasEqualOrAscendingNumbers = number => {
+  let result = true;
+  const arr = number.toString().split('').map(item => parseInt(item));
+  for (let i=0; i<arr.length - 1; i++) {
+    if (arr[i] > arr[i + 1]) {
+      result = false;
+      break;
+    }
+  }
+  return result;
+};
+
+const getMeetingCriteriaPasswords = data => {
+  const inputWithDuplicatesOnly = input.filter(pass => pass.toString().length !== pass.toString().match(/([0-9])(?!.*\1)/gi).length);
+
+
+
+    const noDescendingNumbers = inputWithDuplicatesOnly.filter(pass => hasEqualOrAscendingNumbers(pass));
+
+    console.log(noDescendingNumbers.length, noDescendingNumbers);
+    return noDescendingNumbers.length;
+};
+
+
+fetch(puzzleURL)
+  .then(res => res.text())
+  .then(puzzle => {
+
+    input = handleData(puzzle);
+
+
+    // Part 1
+    result1.innerHTML = getMeetingCriteriaPasswords(input);
+
+
+    // Part 2
+    //result2.innerHTML =
+
+  });
